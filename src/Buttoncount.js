6bc13dc067189ccf736421455933faf2b19b79a8
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-02-25 09:31:40 +1100

    first commit

---
+++ src/Buttoncount.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function App() {
+  let [count, setCount] = useState(0);
+  const addCount = () => {
+    setCount(++count);
+  };
+  const resetCount = () => {
+    if (count > 0) setCount(0);
+  };
+
+  return (
+    <div className="counterDisplay">
+      <header className="App">
+        <h1>Counter</h1>
+
+        <button className="count" onClick={addCount}>
+          Add
+        </button>
+        <div className="displayCount">{count}</div>
+        <button className="reset" onClick={resetCount}>
+          Reset
+        </button>
+      </header>
+    </div>
+  );
+}
